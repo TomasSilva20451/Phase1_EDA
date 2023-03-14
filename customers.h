50f8780884997ae97b41aa1a800d5174e14f0196
TomasSilva20451 <120596669+TomasSilva20451@users.noreply.github.com>
2023-03-14 14:36:59 +0000

    Update customers.h

--- customers.h
+++ customers.h
@@ -1,19 +1,18 @@
 // customers.h 
-#ifndef CUSTOMERS_H
-#define CUSTOMERS_H
+#ifndef customers_h
+#define customers_h
 
 #include "electric_mobility_means.h"
 
-typedef struct
-{
+typedef struct Customers {
     char name[50];
-    char NIF[50];
-    float balance;
-    char address[50];
-} CustomerData;
+    char email[50];
+    int age;
+} Customers;
 
 void insert_data(Customers *customers);
 void remove_data(Customers *customers);
 void change_data(Customers *customers);
+void print_data(Customers customers);
 
-#endif // CUSTOMERS_H
+#endif
