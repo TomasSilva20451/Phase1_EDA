31ade7a8868f7bcaae1ea73b3fdd15f37211d7ac
TomasSilva20451 <a20451@alunos.ipca.pt>
2023-03-15 01:59:14 +0000

    3

--- manager.h
+++ manager.h
@@ -1,22 +1,30 @@
 #ifndef MANAGER_H
 #define MANAGER_H
 
-#define MAX_NAME_LENGTH 50
-#define MAX_EMAIL_LENGTH 50
-#define MAX_PASSWORD_LENGTH 50
+#define MAX_MANAGER_NAME_LENGTH 50
+#define MAX_MANAGER_EMAIL_LENGTH 50
+#define MAX_MANAGER_PASSWORD_LENGTH 50
+#define MAX_MANAGER_TYPE_LENGTH 50
+#define MAX_MANAGER_LOCATION_GEOCODE_LENGTH 50
+#define MAX_MANAGER_DESCRIPTION_LENGTH 100
 
 struct manager {
-    char name[MAX_NAME_LENGTH];
-    char email[MAX_EMAIL_LENGTH];
-    char password[MAX_PASSWORD_LENGTH];
-    float salary;
-    struct manager *next;
+char name[MAX_MANAGER_NAME_LENGTH];
+char email[MAX_MANAGER_EMAIL_LENGTH];
+char password[MAX_MANAGER_PASSWORD_LENGTH];
+char type[MAX_MANAGER_TYPE_LENGTH];
+char location_geocode[MAX_MANAGER_LOCATION_GEOCODE_LENGTH];
+float autonomy;
+float battery_level;
+float cost;
+char description[MAX_MANAGER_DESCRIPTION_LENGTH];
+float salary;
+struct manager *next;
 };
 
+void print_all_managers();
 void add_manager(char *email, char *name, char *password, float salary);
-struct manager *find_manager_by_email(char *email);
-void update_manager_salary(char *email, float new_salary);
-void update_manager_info(char *email, char *new_name, char *new_password);
-void delete_manager(char *email);
+void remove_manager(char *email);
+void update_manager_info(char *email, char *new_name, char *new_password, char *new_type, char *new_location_geocode, float new_autonomy, float new_battery_level, float new_cost, char *new_description);
 
 #endif /* MANAGER_H */
