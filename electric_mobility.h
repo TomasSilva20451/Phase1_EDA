5f95a0faa0e1de78d353e29b731d62c120e6855e
TomasSilva20451 <a20451@alunos.ipca.pt>
2023-03-17 23:29:57 +0000

    19

--- electric_mobility.h
+++ electric_mobility.h
@@ -12,11 +12,13 @@ typedef struct electric_mobility
     float rent;
     float autonomy;
     int battery_level;
-    int max_rental_duration; // novo membro adicionado
+    int max_rental_duration;
+    int battery_level_per_minute;
     struct electric_mobility *next;
 } ElectricMobility;
 
 ElectricMobility *create_electric_mobility(int id, char *name, float price, int stock, float rent, float autonomy, int battery_level);
+
 void destroy_electric_mobility(ElectricMobility *em);
 float get_price_per_kwh(ElectricMobility *em);
 void set_price_per_kwh(ElectricMobility *em, float price_per_kwh);
