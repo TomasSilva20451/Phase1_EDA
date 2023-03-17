1acf9ec83a7a6c9cbd0962905004c1935c9b66cb
TomasSilva20451 <a20451@alunos.ipca.pt>
2023-03-17 17:17:36 +0000

    11

--- client.c
+++ client.c
@@ -5,17 +5,19 @@
 
 struct client *clients_head = NULL;
 
-void add_client(char *name, char *email, char *phone_number, char *nif)
+void add_client(char *name, char *email, int phone_number, int nif)
 {
-    struct client *new_node = malloc(sizeof(struct client*));
+    int aux = phone_number;
+
+    struct client *new_node = malloc(sizeof(struct client *));
     strncpy(new_node->name, name, MAX_NAME_LENGTH - 1);
-    new_node->name[MAX_NAME_LENGTH - 1] = '\0';
+    // new_node->name[MAX_NAME_LENGTH - 1] = '\0';
     strncpy(new_node->email, email, MAX_EMAIL - 1);
-    new_node->email[MAX_EMAIL - 1] = '\0';
-    strncpy(new_node->phone_number, phone_number, MAX_PHONE_NUMBER_LENGTH - 1);
-    new_node->phone_number[MAX_PHONE_NUMBER_LENGTH - 1] = '\0';
-    strncpy(new_node->nif, nif, MAX_NIF_LENGTH - 1);
-    new_node->nif[MAX_NIF_LENGTH - 1] = '\0';
+    // new_node->email[MAX_EMAIL - 1] = '\0';
+    new_node->phone_number = aux;
+    // new_node->phone_number[MAX_PHONE_NUMBER_LENGTH - 1] = '\0';
+    new_node->nif = nif;
+    // new_node->nif[MAX_NIF_LENGTH - 1] = '\0';
     new_node->next = NULL;
 
     if (clients_head == NULL)
@@ -33,14 +35,14 @@ void add_client(char *name, char *email, char *phone_number, char *nif)
     }
 }
 
-void remove_client(char *nif)
+void remove_client(int nif)
 {
     struct client *current = clients_head;
     struct client *previous = NULL;
 
     while (current != NULL)
     {
-        if (strcmp(current->nif, nif) == 0)
+        if (current->nif == nif)
         {
             if (previous == NULL)
             {
@@ -65,26 +67,26 @@ void list_clients()
     {
         printf("Name: %s\n", current->name);
         printf("Email: %s\n", current->email);
-        printf("Phone number: %s\n", current->phone_number);
-        printf("NIF: %s\n", current->nif);
+        printf("Phone number: %d\n", current->phone_number);
+        printf("NIF: %d\n", current->nif);
         printf("\n");
         current = current->next;
     }
 }
 
-void load_clients(char *filename)
+Client *load_clients(char *filename)
 {
     FILE *fp = fopen(filename, "rb");
     if (fp == NULL)
     {
         printf("Error opening file.\n");
-        return;
+        return 0;
     }
 
     char name[MAX_NAME_LENGTH];
     char email[MAX_EMAIL];
-    char phone_number[MAX_PHONE_NUMBER_LENGTH];
-    char nif[MAX_NIF_LENGTH];
+    char phone_number[15];
+    char nif[20];
 
     while (fread(name, MAX_NAME_LENGTH - 1, 1, fp) == 1)
     {
@@ -96,10 +98,11 @@ void load_clients(char *filename)
         fread(nif, MAX_NIF_LENGTH - 1, 1, fp);
         nif[MAX_NIF_LENGTH - 1] = '\0';
 
-        add_client(name, email, phone_number, nif);
+        add_client(name, email, atoi(phone_number), atoi(nif));
     }
 
     fclose(fp);
+    return clients_head;
 }
 /*void save_clients(char *filename)
 {
@@ -119,5 +122,3 @@ void load_clients(char *filename)
     fclose(outfile);
     printf("Saved %d clients to %s\n", num_clients, filename);
 }*/
-
-
--- client.h
+++ client.h
@@ -12,21 +12,21 @@ typedef struct client {
     char name[MAX_NAME_LENGTH];
     char address[MAX_ADDRESS_LENGTH];
     int age;
-    int phone_number[MAX_PHONE_NUMBER_LENGTH];
+    int phone_number;
     char email[MAX_EMAIL]; 
     int account_numbers[MAX_ACCOUNTS];
     double account_balances[MAX_ACCOUNTS];
     int num_accounts;
-    int nif [MAX_NIF_LENGTH];
+    int nif;
     struct client* next;
 } Client;
 
-Client *new_client(char *name, char *address, int age);
+Client *new_client(char *name, char *address, int phone_number, int nif);
 
 void add_account(Client *client, int account_number, double balance);
 
 void save_clients(char *filename, Client **clients, int num_clients);
 
-void *load_clients(char *filename);
+Client *load_clients(char *filename);
 
 #endif /* CLIENT_H */
